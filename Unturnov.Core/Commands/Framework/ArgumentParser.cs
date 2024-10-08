2eec5e133f57134e267c52eb44b3d7c8eb7b44b0
Kana <109441902+ai-kana@users.noreply.github.com>
2024-10-08 15:56:19 -0700

    Add multi argument parsers

--- Unturnov.Core/Commands/Framework/ArgumentParser.cs
+++ Unturnov.Core/Commands/Framework/ArgumentParser.cs
@@ -3,11 +3,11 @@ namespace Unturnov.Core.Commands.Framework;
 public abstract class ArgumentParser<T> : IArgumentParser
 {
     public Type Type => typeof(T);
-    public abstract bool TryParse(string argument, out T result);
+    public abstract bool TryParse(IEnumerator<string> enumerator, out T result);
 
-    public bool TryParseArgument(string argument, out object? result)
+    public bool TryParseArgument(IEnumerator<string> enumerator, out object? result)
     {
-        bool ret = TryParse(argument, out T r );
+        bool ret = TryParse(enumerator, out T r );
         result = r;
         return ret;
     }
